8d2b918be4c4ba69959b22a00fa88f4e71ae1001
Tommy Cheung <tommy200401@gmail.com>
2023-06-15 03:12:22 +0800

    Updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>JLPT Vocab Learning</h1>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          height: '100px',
+          padding: '10px',
+        }}>
+          <Paper elevation={3} sx={{
+            padding: '15px',
+          }}>
+            <h2>ありがとうございます</h2>
+          </Paper>
+        </Box>
     </div>
   );
 }
